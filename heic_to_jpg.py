2032367d4aa05030ed2e6b11013ebd7723fb27a0
ASlicedBread <aslicedbreaaad@gmail.com>
2024-04-26 17:37:11 +0200

    Changed the code to use path given in parameter

--- heic_to_jpg.py
+++ heic_to_jpg.py
@@ -3,52 +3,69 @@ import os
 import subprocess
 import argparse
 import shutil
-
+import pillow_heif
+from PIL import Image
 heic_folder = "heic_files"
 no_folders = False
 debug_mode = False
-sudo_mode = False
 step_print = 25
-convert_types = [".jpg",".png"]
+convert_types = [".jpg", ".png"]
+conv_path = "."
 
 def get_file_count(curr_path: str) -> int:
-    num_files_to_convert = 0
-    for filename in os.listdir(curr_path):
-        if filename.lower().endswith(".heic"):
-            num_files_to_convert += 1
-    return num_files_to_convert
+    return len(list(filter(lambda file: file.lower().endswith(".heic"),os.listdir(curr_path))))
+
+def filter_images(files:list[str])->list[str]:
+    return list(filter(lambda img: img.lower().endswith(".heic"),files))
+
+def print_conversion_result(num_files_to_convert: int, num_files_converted: int,):
+
+    subs_file = num_files_to_convert-num_files_converted
+    if num_files_converted % step_print == 0 and subs_file > 0:
+        print(
+            f"Converted {num_files_converted} files - {subs_file} remaining")
 
 
-def convert_files(curr_path: str,conv_type:str):
-    debug_folder = "debug/" if debug_mode else ""
-    sudo_mode_command = "sudo" if sudo_mode else ""
+def convert_files(curr_path: str, conv_type: str):
     if not no_folders:
-        os.makedirs(debug_folder+heic_folder, exist_ok=True)
+        os.makedirs(os.path.join(curr_path,heic_folder), exist_ok=True)
     num_files_converted = 0
     print("Starting conversion from HEIC to JPG")
+    images = filter_images(os.listdir(curr_path))   
+    print(images)  
     try:
-        for _, filename in enumerate(os.listdir(curr_path)):
-            if filename.lower().endswith(".heic"):
-                debug_folder = "debug/" if debug_mode else ""
-                if (not subprocess.run([sudo_mode_command, "convert", f"{debug_folder}{filename}", f"{debug_folder}{(filename[0:-5] + conv_type)}"]).returncode):
-                    if not no_folders:
-                        if subprocess.run([sudo_mode_command, "mv", f"{debug_folder}{filename}", f"{debug_folder+heic_folder}/"]).returncode:
-                            print(
-                                f"Couldn't move {debug_folder}{filename} to {debug_folder+heic_folder}/")
-                    num_files_converted += 1
-                    subs_file = num_files_to_convert-num_files_converted
-                    if num_files_converted % step_print == 0 and subs_file > 0:
-                        print(
-                            f"Converted {num_files_converted} files - {subs_file} remaining")
-                else:
-                    print(f"Couldn't convert {debug_folder}{filename}")
+        for _, filename in enumerate(images):
+        
+            heic_img = pillow_heif.read_heif(os.path.join(curr_path,filename))
+            try:
+                img = Image.frombytes(
+                    heic_img.mode, heic_img.size, heic_img.data, "raw")
+            except:
+                print(f"Error converting {os.path.join(curr_path,filename)}")
+            else: 
+                try: 
+                    img.save(f"{os.path.join(curr_path,(filename[0:-5] + conv_type))}")
+                except OSError:
+                    print(f"Error while trying to save {curr_path}{(filename[0:-5] + conv_type)}")
+                if not no_folders:
+                    try:
+                        shutil.move(os.path.join(curr_path,filename),
+                                    os.path.join(curr_path,heic_folder))
+                    except shutil.Error:
+                        print(f"Couldn't move {curr_path}{filename} to {os.path.join(curr_path,heic_folder)}")
+                num_files_converted += 1
+                print_conversion_result(
+                    num_files_to_convert, num_files_converted)
     except PermissionError:
         print("Not enough permissions operate")
 
-    if (not os.listdir(debug_folder+heic_folder)):
-        os.rmdir(debug_folder+heic_folder)
-    final_type = conv_type.replace('.',"").upper()
-    print(f"Finished converting {num_files_converted} images from HEIC to {final_type}")
+    if not no_folders:
+        if (not os.listdir(os.path.join(curr_path,heic_folder))):
+            os.rmdir(os.path.join(curr_path,heic_folder))
+    final_type = conv_type.replace('.', "").upper()
+    print(
+        f"Finished converting {num_files_converted} images from HEIC to {final_type}")
+    return num_files_converted
 
 
 if __name__ == "__main__":
@@ -58,36 +75,38 @@ if __name__ == "__main__":
         "--nr", help="Disables the creation of the folder containing the HEIC images after conversion", action="store_true")
     parser.add_argument(
         "--debug", help="For debugging the app", action="store_true")
-    parser.add_argument(
-        "--sm", help="Enables sudo mode for commands", action="store_true")
-    parser.add_argument("--type",default=".jpg",help="Defines the type to convert to (Default: .jpg)",choices=[convert_types])
+    parser.add_argument("--type", default=".jpg",
+                        help="Defines the type to convert to (Default: .jpg)", choices=[convert_types])
+    parser.add_argument("--path", default=".",
+                        help="Specify a path to convert files at")
     args = parser.parse_args()
 
     no_folders = args.nr
     debug_mode = args.debug
-    sudo_mode = args.sm
-    curr_path = os.getcwd()
     conv_type = args.type
+    conv_path = args.path
+    curr_path = os.getcwd()
+
     if debug_mode:
-        os.makedirs("debug", exist_ok=True)
+        conv_path = "./debug"
+        os.makedirs(conv_path, exist_ok=True)
         try:
             for i in range(0, 20):
-                shutil.copy(f"{curr_path}/sample1.heic",
-                            f"{curr_path}/debug/sample_"+str(i)+".heic")
+                shutil.copy(f"{conv_path}/../sample1.heic",
+                            f"{conv_path}/sample_"+str(i)+".heic")
         except:
             print("Debug files already present")
-    debug_folder = "debug/" if debug_mode else ""
 
-    os.chdir(curr_path)
-    num_files_to_convert = get_file_count(curr_path+"/"+debug_folder)
-    print(f"Found {num_files_to_convert} files to convert")
-    if num_files_to_convert > 0:
-        step_print = round(num_files_to_convert*0.10)
-        if step_print <= 0:
-            step_print = 1
-        if debug_mode:
-            print("DEBUG MODE")
-            
-            convert_files(f"{curr_path}/debug",conv_type)
-        else:
-            convert_files(curr_path,conv_type)
+    if os.path.exists(conv_path):
+        num_files_to_convert = get_file_count(conv_path)
+        print(f"Found {num_files_to_convert} files to convert")
+        if num_files_to_convert > 0:
+            step_print = round(num_files_to_convert*0.10)
+            if step_print <= 0:
+                step_print = 1
+            if debug_mode:
+                print("DEBUG MODE")
+                conv_path = "debug/"
+                convert_files(conv_path,conv_type)
+            else:
+                convert_files(conv_path, conv_type)
