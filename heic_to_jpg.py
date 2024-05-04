1eac9a64a8a11e8cfade1d113a69f2a8df96086e
ASlicedBread <aslicedbreaaad@gmail.com>
2024-05-04 04:25:53 +0200

    Added multiprocessing (No more print for steps though)

--- heic_to_jpg.py
+++ heic_to_jpg.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3.10
+from multiprocessing import Process, Queue
 import os
 import argparse
 import shutil
 import pillow_heif
 from PIL import Image
 from pathlib import Path
+import time
 heic_folder = "heic_files"
 no_folders = False
 debug_mode = False
 step_print = 25
 convert_types = [".jpg", ".png"]
 conv_path = "."
+num_procs = 75
+
 
 def get_file_count(curr_path: str) -> int:
     return len(list(filter(lambda file: file.lower().endswith(".heic"),os.listdir(curr_path))))
@@ -25,15 +29,9 @@ def print_conversion_result(num_files_to_convert: int, num_files_converted: int,
         print(
             f"Converted {num_files_converted} files - {subs_file} remaining")
 
-
-def convert_files(curr_path: str, conv_type: str):
-    if not no_folders:
-        os.makedirs(os.path.join(curr_path,heic_folder), exist_ok=True)
+def conversion_process(images:list[str],curr_path:str,queue:Queue):
     num_files_converted = 0
-    print("Starting conversion from HEIC to JPG")
-    images = filter_images(os.listdir(curr_path))   
-    try:
-        for filename in images:
+    for filename in images:
             try:
                 heic_img = pillow_heif.read_heif(os.path.join(curr_path,filename))
                 img = Image.frombytes(
@@ -44,7 +42,7 @@ def convert_files(curr_path: str, conv_type: str):
                 new_file = Path(filename).stem + conv_type
                 try: 
                     img.save(f"{os.path.join(curr_path,(new_file))}")
-                except OSError as e:
+                except Exception as e:
                     print(f"Error while trying to save {os.path.join(curr_path,(new_file))}:",e)
                 if not no_folders:
                     try:
@@ -53,19 +51,46 @@ def convert_files(curr_path: str, conv_type: str):
                     except shutil.Error as e:
                         print(f"Couldn't move {os.path.join(curr_path,filename)} to {os.path.join(curr_path,heic_folder)}:",e)
                 num_files_converted += 1
-                print_conversion_result(
-                    num_files_to_convert, num_files_converted)
+    queue.put(num_files_converted)
+
+
+def convert_files(curr_path: str, conv_type: str):
+    num_files_converted = 0
+    if not no_folders:
+        os.makedirs(os.path.join(curr_path,heic_folder), exist_ok=True)
+    print("Starting conversion from HEIC to JPG")
+    images = filter_images(os.listdir(curr_path))   
+    try:
+        dic = {}
+        q = Queue()
+        processes :list[Process] = []
+        for i in range(num_procs):
+            temp = []
+            for id,elem in enumerate(images):
+                if id % num_procs == i:
+                    temp.append(elem)
+            dic[i] = temp.copy()
+        for i in range(num_procs):
+            proc = Process(target=conversion_process, args=(dic[i],curr_path,q))
+            processes.append(proc)
+            proc.start()
+
+        for proc in processes:
+            proc.join()
+        
+        while not q.empty() :
+            num_files_converted+= q.get_nowait()
     except PermissionError as e:
         print("Not enough permissions operate:",e)
     except Exception as e:
         print("An error has occured:",e)
-
-    if not no_folders:
-        if (not os.listdir(os.path.join(curr_path,heic_folder))):
-            os.rmdir(os.path.join(curr_path,heic_folder))
-    final_type = conv_type.replace('.', "").upper()
-    print(
-        f"Finished converting {num_files_converted} images from HEIC to {final_type}")
+    finally:
+        if not no_folders:
+            if (not os.listdir(os.path.join(curr_path,heic_folder))):
+                os.rmdir(os.path.join(curr_path,heic_folder))
+        final_type = conv_type.replace('.', "").upper()
+        print(
+            f"Finished converting {num_files_converted} images from HEIC to {final_type}")
     return num_files_converted
 
 
@@ -80,18 +105,23 @@ if __name__ == "__main__":
                         help="Defines the type to convert to (Default: .jpg)", choices=[convert_types])
     parser.add_argument("--path", default=".",
                         help="Specify a path to convert files at")
+    parser.add_argument("--np",default=75,help="Specify the number of processes to use in multiprocessing",type=int)
     args = parser.parse_args()
 
     no_folders = args.nr
     debug_mode = args.debug
     conv_type = args.type
     conv_path = args.path
+    num_procs = args.np
+
+        
+    
 
     if debug_mode:
         conv_path = "./debug"
         os.makedirs(conv_path, exist_ok=True)
         try:
-            for i in range(0, 20):
+            for i in range(0, 250):
                 shutil.copy(f"{conv_path}/../debug_resources/sample1.heic",
                             f"{conv_path}/sample_"+str(i)+".heic")
         except:
@@ -99,16 +129,19 @@ if __name__ == "__main__":
 
     if os.path.exists(conv_path):
         num_files_to_convert = get_file_count(conv_path)
+        num_procs = num_files_to_convert if num_procs > num_files_to_convert else num_procs
         print(f"Found {num_files_to_convert} files to convert")
         if num_files_to_convert > 0:
             step_print = round(num_files_to_convert*0.10)
             if step_print <= 0:
                 step_print = 1
+            time_start = time.time()
             if debug_mode:
                 print("DEBUG MODE")
                 conv_path = "debug/"
                 convert_files(conv_path,conv_type)
             else:
                 convert_files(conv_path, conv_type)
+            print(f"Took {time.time()-time_start} seconds")
     else:
         print(f"{conv_path} doesn't exist")
